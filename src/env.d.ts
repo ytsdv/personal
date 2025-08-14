19a654520c295ed95f774f0a361aa125a7684b55
tim <mr.tim9191@gmail.com>
2025-08-14 16:39:25 +0200

    add theme

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,17 @@
+/// <reference types="astro/client" />
+/// <reference types="astro/content" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
   interface Locals extends Runtime {}
+}
+
+declare module 'astro:content' {
+  interface Render {
+    '.md': Promise<{
+      Content: import('astro').MarkdownInstance<Record<string, unknown>>['Content']
+      headings: import('astro').MarkdownHeading[]
+      remarkPluginFrontmatter: Record<string, unknown>
+    }>
+  }
 }
